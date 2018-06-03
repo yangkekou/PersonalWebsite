352263efe8235716796c947c46783250edd83e14
杨克扣 <910217370@qq.com>
2018-06-03 11:01:56 +0800

    this time I have make the personal nav bar

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
 window.onload=function(){
 
+//make the nav buttom has listener
+var listener=PersonalNav();
+document.querySelectorAll(".personal-nav li").forEach(
+     function(e){
+     	let el=e.dataset.bind;
+     	if(el!="item-personal")
+        document.getElementsByClassName(el)[0].style.display="none";
+     	e.addEventListener("click",listener);
+     }
+	)
 	
 }
 
@@ -11,4 +21,19 @@ function active(element){
 
 function off(element){
 	element.style.display="none";
+}
+
+
+
+function PersonalNav(){
+   let current=document.getElementsByClassName("item-personal")[0];
+   current.style.display=null;
+   return  function(event){
+   	    if(current&&current!=this)
+   	    current.style.display="none";
+   		else
+   		retrun;
+   		current=document.getElementsByClassName(this.dataset.bind)[0];
+   		current.style.display=null;
+   };
 }
