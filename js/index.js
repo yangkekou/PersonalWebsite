1e901f3b7d31008ebf6a9246cae013baa1d82bc6
杨克扣 <910217370@qq.com>
2018-06-06 16:37:29 +0800

    the next stp I will add the icons

--- js/index.js
+++ js/index.js
@@ -1,4 +1,25 @@
+var p_height=window.innerHeight-110;
+
+
+//this function is used to init the status of my website,for example,some div's height and etc.
+function innit(){
+
+document.querySelectorAll(".resume>div").forEach(
+     function(e){
+     	e.style.height=p_height+"px";
+     }
+	);
+
+document.querySelectorAll('skills>div').forEach(function(e){
+ e.style.display=none;
+});
+
+}
+
+
+
 window.onload=function(){
+	innit();
 
 //make the nav buttom has listener
 var listener=PersonalNav();
@@ -9,10 +30,19 @@ document.querySelectorAll(".personal-nav li").forEach(
         document.getElementsByClassName(el)[0].style.display="none";
      	e.addEventListener("click",listener);
      }
-	)
-	
-}
+	);
 
+
+//this is works for skill nav,there are something the same with above;
+
+var skillListener=skillNav();
+document.querySelectorAll('.items>div').forEach(
+	function(e){
+        e.addEventListener('click',skillListener);
+	}
+
+	);
+}
 
 
 function active(element){
@@ -27,13 +57,41 @@ function off(element){
 
 function PersonalNav(){
    let current=document.getElementsByClassName("item-personal")[0];
+   let clicked=document.querySelectorAll(".personal-nav li")[0];
    current.style.display=null;
+   clicked.style.color="#ff4547";
+   current.classList.add('enter_start');
+   current.classList.add('enter_animater');
+   setTimeout(()=>current.classList.add('enter_end'),1000);
    return  function(event){
-   	    if(current&&current!=this)
-   	    current.style.display="none";
-   		else
-   		retrun;
+   	    if(current.className==this.dataset.bind)
+ 			return;
+
+   	   	current.style.display="none";
+   	   	current.classList.remove("enter_start");
+   	   	current.classList.remove("enter_end");
+   	   	current.classList.remove("enter_animater");
+   	    clicked.style.color=null;  
+
    		current=document.getElementsByClassName(this.dataset.bind)[0];
+   		clicked=this;
+   		current.classList.add('enter_start');
+   		current.classList.add('enter_animater');
+   		setTimeout(()=>current.classList.add('enter_end'),1000);
    		current.style.display=null;
+   		this.style.color="#ff4547";
    };
 }
+
+
+function skillNav(){
+	let current=document.querySelector('.skill-language');
+    current.style.display=null;
+	return function(event){
+ 		current.style.display
+		if(this.dataset.bind.split(',').length!=1)
+		alert(this.dataset.bind.split(',')[1])
+		alert(this.dataset.bind);
+	};
+}
+
